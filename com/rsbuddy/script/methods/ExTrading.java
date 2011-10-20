2733a0a829f745094dd9927870a5fa89eae1e1f8
123dsw666 <123dsw666@googlemail.com>
2011-10-20 14:30:08 +0000

    Rewrote ExTrading. Added door handling to ExWalking. ExWalking will soon handle ladders, stairs, boats and teleports!

--- com/rsbuddy/script/methods/ExTrading.java
+++ com/rsbuddy/script/methods/ExTrading.java
@@ -8,60 +8,53 @@ import com.rsbuddy.script.wrappers.Item;
 import com.rsbuddy.script.wrappers.Player;
 import com.rsbuddy.script.wrappers.Widget;
 
+import java.util.LinkedList;
+
 import org.rsbuddy.tabs.Inventory;
 
 /**
- * @author TantricBots
  * @author Ramus
  */
 public class ExTrading {
 
-	public static class AcceptBox {
-
-		public static class getTraderName {
-
-			public static String name() {
-				if (!isOpen()) {
-					return null;
-				}
-				return getWidget().getComponent(WIDGET_TRADE_TRADER).getText();
-			}
-		}
+	public static class AcceptScreen {
 
 		public static final int WIDGET_TRADE = 334;
-		public static final int WIDGET_TRADE_BUTTON_CLOSE = 6;
+		public static final int WIDGET_TRADE_ACCEPTED = 34;
 		public static final int WIDGET_TRADE_BUTTON_ACCEPT = 36;
+		public static final int WIDGET_TRADE_BUTTON_CLOSE = 6;
 		public static final int WIDGET_TRADE_BUTTON_DECLINE = 37;
 		public static final int WIDGET_TRADE_TRADER = 54;
-		public static final int WIDGET_TRADE_ACCEPTED = 34;
 
 		public static boolean accept() {
 			if (!isOpen()) {
 				return false;
 			}
 			if (getWidget().getComponent(WIDGET_TRADE_ACCEPTED).containsText("Waiting for other")) {
-				Task.sleep(250);
 				return true;
-			} else {
-				final Component button = getWidget().getComponent(WIDGET_TRADE_BUTTON_ACCEPT);
-				return button != null && button.click();
 			}
+			return getWidget().getComponent(WIDGET_TRADE_BUTTON_ACCEPT).interact("Accept");
 		}
 
 		public static boolean close() {
 			if (!isOpen()) {
-				return false;
+				return true;
 			}
-			final Component button = getWidget().getComponent(WIDGET_TRADE_BUTTON_CLOSE);
-			return button != null && button.click();
+			return getWidget().getComponent(WIDGET_TRADE_BUTTON_CLOSE).interact("Close");
 		}
 
 		public static boolean decline() {
-			if (isOpen()) {
-				return false;
+			if (!isOpen()) {
+				return true;
 			}
-			final Component button = getWidget().getComponent(WIDGET_TRADE_BUTTON_DECLINE);
-			return button != null && button.click();
+			return getWidget().getComponent(WIDGET_TRADE_BUTTON_DECLINE).interact("Decline");
+		}
+
+		public static Player getTrader() {
+			if (!isOpen()) {
+				return null;
+			}
+			return Players.getNearest(getWidget().getComponent(WIDGET_TRADE_TRADER).getText());
 		}
 
 		public static Widget getWidget() {
@@ -69,79 +62,134 @@ public class ExTrading {
 		}
 
 		public static boolean isOpen() {
-			return isValid() && getWidget().getComponent(1).isVisible();
-		}
-
-		public static boolean isValid() {
 			final Widget w = getWidget();
-			return w != null && w.isValid();
+			return w != null && w.isValid() && getWidget().getComponent(0).isVisible();
 		}
-
 	}
 
-	public static class OfferBox {
-
-		public static class getTraderName {
-
-			public static boolean isValid(final String playerName) {
-				return playerName.equals(name());
-			}
-
-			public static String name() {
-				if (!isOpen()) {
-					return null;
-				}
-				return getWidget().getComponent(WIDGET_TRADE_TRADER).getText().replace("Trading With: ", "");
-			}
-		}
+	public static class OfferScreen {
 
 		public static final int WIDGET_TRADE = 335;
-		public static final int WIDGET_TRADE_BUTTON_CLOSE = 12;
-		public static final int WIDGET_TRADE_BUTTON_ACCEPT = 16;
-		public static final int WIDGET_TRADE_BUTTON_DECLINE = 18;
-		public static final int WIDGET_TRADE_TRADER = 15;
 		public static final int WIDGET_TRADE_ACCEPTED = 37;
-		public static final int[] WIDGET_TRADE_SLOTS = { 30, 0, 1 };
+		public static final int WIDGET_TRADE_BUTTON_ACCEPT = 16;
+		public static final int WIDGET_TRADE_BUTTON_CLOSE = 12;
+		public static final int WIDGET_TRADE_BUTTON_DECLINE = 18;
+		public static final int WIDGET_TRADE_MY_ITEMS = 30;
+		public static final int WIDGET_TRADE_FREE_SLOTS = 21;
+		public static final int WIDGET_TRADE_TRADER_ITEMS = 33;
+		public static final int WIDGET_TRADE_TRADER = 22;
 
 		public static boolean accept() {
 			if (!isOpen()) {
 				return false;
 			}
 			if (getWidget().getComponent(WIDGET_TRADE_ACCEPTED).containsText("Waiting for other")) {
-				Task.sleep(250);
 				return true;
-			} else {
-				final Component button = getWidget().getComponent(WIDGET_TRADE_BUTTON_ACCEPT);
-				return button != null && button.click();
 			}
+			return getWidget().getComponent(WIDGET_TRADE_BUTTON_ACCEPT).interact("Accept");
 		}
 
 		public static boolean close() {
 			if (!isOpen()) {
-				return false;
+				return true;
 			}
-			final Component button = getWidget().getComponent(WIDGET_TRADE_BUTTON_CLOSE);
-			return button != null && button.click();
+			return getWidget().getComponent(WIDGET_TRADE_BUTTON_CLOSE).interact("Close");
 		}
 
 		public static boolean decline() {
 			if (!isOpen()) {
-				return false;
+				return true;
 			}
-			final Component button = getWidget().getComponent(WIDGET_TRADE_BUTTON_DECLINE);
-			return button != null && button.click();
+			return getWidget().getComponent(WIDGET_TRADE_BUTTON_DECLINE).interact("Decline");
 		}
 
-		public static boolean getItemsTraded() {
-			int i = 1;
-			while (i < 3) {
-				if (getWidget().getComponent(WIDGET_TRADE_SLOTS[0]).getComponent(WIDGET_TRADE_SLOTS[i]).getItemId() > 0) {
-					items[i] = getWidget().getComponent(WIDGET_TRADE_SLOTS[0]).getComponent(WIDGET_TRADE_SLOTS[i])
-							.getItemId();
-				}
-				i += 1;
+		public static int getFreeSlots() {
+			if (!isOpen()) {
+				return 0;
 			}
-			return true;
+			final String text = getWidget().getComponent(WIDGET_TRADE_FREE_SLOTS).getText().substring(4, 6).trim();
+			try {
+				return Integer.parseInt(text);
+			} catch (final Exception e) {
+			}
+			return 0;
+		}
+
+		public static Item[] getMyItems() {
+			if (!isOpen()) {
+				return null;
+			}
+			final Component itemsComp = getWidget().getComponent(WIDGET_TRADE_MY_ITEMS);
+			final LinkedList<Item> items = new LinkedList<Item>();
+			for (int i = 0; i < 28; i += 1) {
+				final Component item = itemsComp.getComponent(i);
+				if (item == null || item.getItemId() == -1 || item.getItemStackSize() == 0) {
+					continue;
+				}
+				items.add(new Item(item));
+			}
+			return items.toArray(new Item[items.size()]);
+		}
+
+		public static Item[] getTraderItems() {
+			if (!isOpen()) {
+				return null;
+			}
+			final Component itemsComp = getWidget().getComponent(WIDGET_TRADE_TRADER_ITEMS);
+			final LinkedList<Item> items = new LinkedList<Item>();
+			for (int i = 0; i < 28; i += 1) {
+				final Component item = itemsComp.getComponent(i);
+				if (item == null || item.getItemId() == -1 || item.getItemStackSize() == 0) {
+					continue;
+				}
+				items.add(new Item(item));
+			}
+			return items.toArray(new Item[items.size()]);
+		}
+
+		public static boolean offer(final int id, final int count) {
+			if (!isOpen()) {
+				return false;
+			}
+			final Item item = Inventory.getItem(id);
+			final int startCount = Inventory.getCount(true, id);
+			final int endCount = count == 0 ? 0 : startCount - count;
+			if (startCount == 0) {
+				return false;
+			}
+			if (count == 0) {
+				if (!item.interact(startCount > 1 ? "Offer-All" : "Offer")) {
+					return false;
+				}
+			} else if (count == 1) {
+				if (!item.interact("Offer")) {
+					return false;
+				}
+			} else {
+				if (!item.interact("Offer-" + count)) {
+					if (item.interact("Offer-X")) {
+						Task.sleep(1000, 1300);
+						Keyboard.sendText(String.valueOf(count), true);
+					} else {
+						return false;
+					}
+				}
+			}
+			Action.sleep(2500, new Condition() {
+
+				@Override
+				public boolean isValid() {
+					return Inventory.getCount(id) == endCount;
+				}
+			});
+			return Inventory.getCount(id) == endCount;
+		}
+
+		public static Player getTrader() {
+			if (!isOpen()) {
+				return null;
+			}
+			return Players.getNearest(getWidget().getComponent(WIDGET_TRADE_TRADER).getText());
 		}
 
 		public static Widget getWidget() {
@@ -149,64 +197,63 @@ public class ExTrading {
 		}
 
 		public static boolean isOpen() {
-			return isValid() && getWidget().getComponent(1).isVisible();
-		}
-
-		public static boolean isValid() {
 			final Widget w = getWidget();
-			return w != null && w.isValid();
+			return w != null && w.isValid() && getWidget().getComponent(0).isVisible();
 		}
+	}
 
-		public static boolean offer(final int item, final int count) {
-			final Item inv = Inventory.getItem(item);
-			final int i = Inventory.getCount(true, item);
-			if (count == 0 && i > 0) {
-				inv.interact("Offer-All");
-				Action.sleep(1000, new Condition() {
+	public enum TradeScreen {
 
-					@Override
-					public boolean isValid() {
-						return Inventory.getCount(item) == 0;
-					}
-				});
-				return true;
-			} else if (i >= count) {
-				inv.interact("Offer-X");
-				Action.sleep(1000, new Condition() {
+		CLOSED,
+		OFFERS,
+		FINAL;
+	}
 
-					@Override
-					public boolean isValid() {
-						return Inventory.getCount(item) == i - count;
-					}
-				});
-			}
+	public static int WIDGET_CHAT = 137;
+	public static int WIDGET_CHAT_COMPONENT = 58;
+
+	public static boolean acceptTradeFrom(final String name) {
+		if (OfferScreen.isOpen()) {
+			return OfferScreen.getTrader().getName().equalsIgnoreCase(name);
+		} else if (AcceptScreen.isOpen()) {
+			return AcceptScreen.getTrader().getName().equalsIgnoreCase(name);
+		}
+		final Component talkComponent = Widgets.getComponent(WIDGET_CHAT, WIDGET_CHAT_COMPONENT);
+		if (talkComponent == null || !talkComponent.isValid() || !talkComponent.isVisible()) {
 			return false;
 		}
-	}
-
-	public enum TransactionScreen {
-		CLOSED,
-		FIRST,
-		SECOND
-	}
-
-	public static int[] items = { 0, 0, 0 };
-
-	public static TransactionScreen getTransactionScreen() {
-		if (OfferBox.isOpen()) {
-			return TransactionScreen.FIRST;
-		} else if (AcceptBox.isOpen()) {
-			return TransactionScreen.SECOND;
+		for (int i = 0; i < talkComponent.getComponentCount(); i += 1) {
+			final Component msg = talkComponent.getComponent(i);
+			if (msg == null || !msg.isValid() || !msg.isVisible()) {
+				continue;
+			}
+			return msg.interact("Accept trade");
 		}
-		return TransactionScreen.CLOSED;
+		return false;
 	}
 
-	public static void tradeWith(final String name) {
+	public static TradeScreen getCurrentScreen() {
+		if (OfferScreen.isOpen()) {
+			return TradeScreen.OFFERS;
+		} else if (AcceptScreen.isOpen()) {
+			return TradeScreen.FINAL;
+		}
+		return TradeScreen.CLOSED;
+	}
+
+	public static boolean tradeWith(final String name) {
+		if (OfferScreen.isOpen()) {
+			return OfferScreen.getTrader().getName().equalsIgnoreCase(name);
+		} else if (AcceptScreen.isOpen()) {
+			return AcceptScreen.getTrader().getName().equalsIgnoreCase(name);
+		}
 		final Player t = Players.getNearest(name);
-		if (t != null && t.interact("Trade with " + name)) {
-			Task.sleep(3000, 3500);
-			return;
+		if (t == null || t.interact("Trade with " + name)) {
+			return false;
 		}
-		return;
+		for (int i = 0; !Game.getLastMessage().toLowerCase().contains("sending trade offer...") && i < 10; i += 1) {
+			Task.sleep(250, 300);
+		}
+		return Game.getLastMessage().toLowerCase().contains("sending trade offer...");
 	}
 }
--- com/rsbuddy/script/methods/ExWalking.java
+++ com/rsbuddy/script/methods/ExWalking.java
@@ -1,18 +1,128 @@
 package com.rsbuddy.script.methods;
 
+import com.rsbuddy.script.task.Task;
+import com.rsbuddy.script.util.Filter;
 import com.rsbuddy.script.util.Random;
 import com.rsbuddy.script.wrappers.Area;
+import com.rsbuddy.script.wrappers.GameObject;
 import com.rsbuddy.script.wrappers.LocalPath;
-import com.rsbuddy.script.wrappers.Path.TraversalOption;
 import com.rsbuddy.script.wrappers.Tile;
 import com.rsbuddy.script.wrappers.TilePath;
 
-import java.util.EnumSet;
+import org.rsbuddy.tabs.Inventory;
+import org.rsbuddy.tabs.Magic;
+import org.rsbuddy.tabs.Magic.Book;
+import org.rsbuddy.tabs.Magic.Spell;
+import org.rsbuddy.tabs.Quest;
 
 /**
  * @author Ramus
  */
 public class ExWalking {
+
+	public static enum Ferry {
+
+		KARAMJA("Karamja", 30, null, null, null);
+
+		private final String name;
+		private final int cost;
+		private final Area port;
+		private final Tile location;
+		private final String quest;
+
+		private Ferry(final String name, final int cost, final String quest, final Area port, final Tile location) {
+			this.name = name;
+			this.cost = cost;
+			this.quest = quest;
+			this.port = port;
+			this.location = location;
+		}
+
+		public int getCost() {
+			return cost;
+		}
+
+		public Tile getLocation() {
+			return location;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public Area getPort() {
+			return port;
+		}
+
+		public String getQuest() {
+			return quest;
+		}
+	}
+
+	public static enum Teleport {
+
+		LUMBRIDGE("Lumbridge", Magic.Modern.LUMBRIDGE_TELEPORT, Book.MODERN, null, null);
+
+		private final String name;
+		private final Spell spell;
+		private final Area location;
+
+		private Teleport(final String name, final Spell spell, final Book book, final String quest, final Area location) {
+			this.name = name;
+			this.spell = spell;
+			this.location = location;
+		}
+
+		public Area getLocation() {
+			return location;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public Spell getSpell() {
+			return spell;
+		}
+	}
+
+	private static final String[] OBJECT_NAMES = { "ladder", "stairs", "stair case", "door", "gate", "stile" };
+	private static final String[] OBJECT_ACTIONS = { "open", "climb-up", "climb-down", "climb-over" };
+	private static String action = null;
+
+	private static boolean canTakeFerry(final Ferry ferry) {
+		return ferry != null && (ferry.getQuest() == null || Quest.isCompleted(ferry.getQuest()));
+	}
+
+	private static boolean canTeleport(final Teleport tele) {
+		return tele != null && Skills.getRealLevel(Skills.MAGIC) < tele.getSpell().getLevel();
+	}
+
+	private static Ferry getBestFerry(final Tile dest) {
+		double dist = Calculations.distanceTo(dest);
+		Ferry ferry = null;
+		for (final Ferry f : Ferry.values()) {
+			final double tmp_dist;
+			if ((tmp_dist = Calculations.distanceBetween(dest, f.getLocation())) < dist) {
+				dist = tmp_dist;
+				ferry = f;
+			}
+		}
+		return ferry;
+	}
+
+	private static Teleport getBestTeleport(final Tile dest) {
+		Teleport tele = null;
+		double dist = Calculations.distanceTo(dest);
+		for (final Teleport t : Teleport.values()) {
+			final double tmp_dist;
+			if ((tmp_dist = Calculations.distanceBetween(dest, t.getLocation().getCentralTile())) < dist) {
+				dist = tmp_dist;
+				tele = t;
+			}
+		}
+		return tele;
+	}
 
 	/**
 	 * Gets the closest tile to the specified tile.
@@ -23,17 +133,17 @@ public class ExWalking {
 	 */
 	public static Tile getClosest(final Tile tile) {
 		final LocalPath localPath = (LocalPath) Walking.findPath(tile);
-		if (localPath.getNext() == null) {
-			return tile;
+		if (localPath == null || localPath.getNext() == null) {
+			return Walking.getTileOnMap(tile);
 		}
 		final TilePath tilePath = localPath.getCurrentTilePath();
 		if (tilePath.getNext() == null) {
-			return tile;
+			return Walking.getTileOnMap(tile);
 		}
 		final Tile[] tiles = tilePath.toArray();
 		final Tile next = getNext(tiles);
 		if (next == null) {
-			return tile;
+			return Walking.getTileOnMap(tile);
 		}
 		return next;
 	}
@@ -66,6 +176,24 @@ public class ExWalking {
 		return tiles[Random.nextInt(0, tiles.length)];
 	}
 
+	private static boolean hasCoinsForFerry(final Ferry ferry) {
+		return ferry != null && Inventory.getCount(995) >= ferry.getCost();
+	}
+
+	private static boolean hasItemsToTeleport(final Teleport tele) {
+		if (tele == null) {
+			return false;
+		}
+		for (int i = 0; i < tele.getSpell().getRunes().length; i += 1) {
+			final int ids[] = tele.getSpell().getRunes()[i];
+			final int amount = tele.getSpell().getAmounts()[i];
+			if (Inventory.getCount(ids) < amount) {
+				return false;
+			}
+		}
+		return true;
+	}
+
 	/**
 	 * Reverses the specified tile array.
 	 * 
@@ -81,6 +209,102 @@ public class ExWalking {
 		return rev;
 	}
 
+	private static boolean sail(final Ferry ferry) {
+		final Tile dest = ferry.getLocation();
+		if (dest == null) {
+			return false;
+		}
+		if (!dest.isOnScreen()) {
+			final Teleport tele = getBestTeleport(dest);
+			if (tele != null) {
+				return teleport(tele);
+			}
+			return traverse(dest);
+		}
+		if (!hasCoinsForFerry(ferry)) {
+			// TODO Bank for coins..
+			return false;
+		}
+		return false; // TODO Sail...
+	}
+
+	private static boolean teleport(final Teleport tele) {
+		if (!hasItemsToTeleport(tele)) {
+			// TODO Bank for items..
+			return false;
+		}
+		if (!Magic.castSpell(tele.getSpell())) {
+			return false;
+		}
+		for (int i = 0; !tele.getLocation().contains(Players.getLocal().getLocation()) && i < 10; i += 1) {
+			Task.sleep(250);
+		}
+		return tele.getLocation().contains(Players.getLocal().getLocation());
+	}
+
+	private static boolean traverse(final Tile dest) {
+		final LocalPath localPath = (LocalPath) Walking.findPath(dest);
+		if (localPath == null) {
+			return false;
+		}
+		if (localPath.isValid()) {
+			if (localPath.getEnd().isOnScreen()) {
+				return localPath.getEnd().interact("Walk here");
+			} else if (localPath.getNext().isOnScreen()) {
+				return localPath.getEnd().interact("Walk here");
+			}
+			return localPath.traverse();
+		}
+		final GameObject block = Objects.getNearest(new Filter<GameObject>() {
+
+			@Override
+			public boolean accept(final GameObject go) {
+				if (go == null || go.getDef() == null || go.getDef().getActions() == null
+						|| go.getDef().getName() == null) {
+					return false;
+				}
+				int i = 0;
+				for (final String name : OBJECT_NAMES) {
+					if (go.getDef().getName().equalsIgnoreCase(name)) {
+						i = 1;
+						break;
+					}
+				}
+				if (i == 0) {
+					return false;
+				}
+				for (final String action : OBJECT_ACTIONS) {
+					for (final String act : go.getDef().getActions()) {
+						if (action.equalsIgnoreCase(act)) {
+							ExWalking.action = act;
+							return true;
+						}
+					}
+				}
+				return false;
+			}
+		});
+		if (Calculations.distanceTo(dest) < Calculations.distanceTo(block)) {
+			return traverse(dest);
+		}
+		if (block == null || action == null) {
+			return false;
+		}
+		if (!block.isOnScreen()) {
+			if (block.getLocation().isOnScreen()) {
+				return localPath.getEnd().interact("Walk here");
+			}
+			return block.getLocation().clickOnMap();
+		}
+		if (!block.interact(action)) {
+			return false;
+		}
+		for (int i = 0; block != null && i < 10; i += 1) {
+			Task.sleep(250);
+		}
+		return traverse(dest);
+	}
+
 	/**
 	 * Walks the specified tile array.
 	 * 
@@ -94,7 +318,7 @@ public class ExWalking {
 		if (next == null) {
 			return false;
 		}
-		return Walking.findPath(next).traverse(EnumSet.of(TraversalOption.HANDLE_RUN, TraversalOption.SPACE_ACTIONS));
+		return walkTo(next);
 	}
 
 	/**
@@ -116,28 +340,36 @@ public class ExWalking {
 	/**
 	 * Walks to the specified tile. This handles running.
 	 * 
-	 * @param tile
+	 * @param dest
 	 *            The tile to walk to.
 	 * @return <tt>true</tt> if the specified tile was reached; <tt>false</tt>
 	 *         otherwise.
 	 */
-	public static boolean walkTo(final Tile tile) {
-		final LocalPath localPath = (LocalPath) Walking.findPath(tile);
+	public static boolean walkTo(final Tile dest) {
+		// final Ferry ferry = getBestFerry(dest);
+		// if (ferry != null && canTakeFerry(ferry)) {
+		// return sail(ferry);
+		// }
+		// final Teleport tele = getBestTeleport(dest);
+		// if (tele != null && canTeleport(tele)) {
+		// return teleport(tele);
+		// }
+		final LocalPath localPath = (LocalPath) Walking.findPath(dest);
 		if (localPath == null || localPath.getNext() == null) {
-			return false;
+			return traverse(dest);
 		}
 		final TilePath tilePath = localPath.getCurrentTilePath();
-		if (tilePath == null || tilePath.getNext() == null) {
-			return false;
+		if (tilePath == null) {
+			return traverse(dest);
 		}
 		final Tile[] tiles = tilePath.toArray();
 		if (tiles == null) {
-			return false;
+			return traverse(dest);
 		}
 		final Tile next = getNext(tiles);
 		if (next == null) {
-			return false;
+			return traverse(dest);
 		}
-		return Walking.findPath(next).traverse(EnumSet.of(TraversalOption.HANDLE_RUN, TraversalOption.SPACE_ACTIONS));
+		return traverse(next);
 	}
 }
